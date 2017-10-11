2fcd70eaa3a30b416019b55742910af59fa72c80
Katelyn Stamas <cakelyn@gmail.com>
2017-10-11 19:04:36 -0400

    added functionality to enqueue in queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,36 @@
 var Queue = function() {
   var someInstance = {};
 
+
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = 0;
+  var end = 0;
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (object.keys(storage).length > 0){
+      storage[end + 1] = value;
+    } else {
+      storage[1] = value;
+    }
+    end++;
+
+      // increment end of storag and place value there
   };
 
   someInstance.dequeue = function() {
+    // edit start variable
   };
 
   someInstance.size = function() {
+    // end - start + 1
   };
 
   return someInstance;
 };
+
+var line =  Queue();
+link.enqueue("milk");
