de80fb7e5babbf9bb078cb84f807811dad1e7fc5
Katelyn Stamas <cakelyn@gmail.com>
2017-10-12 22:44:02 -0400

    completed linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    var empty = false;
+    var prevNode = list.tail;
+
+    list.head === null ? empty = true : empty = false;
+    list[node] = node;
+    list.tail = list[node];
+    empty ? list.head = list[node] : prevNode.next = list[node];
   };
 
   list.removeHead = function() {
+    var prevHead = list.head;
+
+    list.head = list.head.next;
+
+    return prevHead.value;
   };
 
   list.contains = function(target) {
+
+    for (var key in list) {
+      if (list[key].value === target) {
+        return true;
+      }
+    }
+
+    return false;
   };
 
   return list;
