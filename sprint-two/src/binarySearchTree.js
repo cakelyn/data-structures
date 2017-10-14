24c6c8bf0fe75d1e103c5a3c9b9c5994a9560c74
Katelyn Stamas <cakelyn@gmail.com>
2017-10-13 23:56:28 -0400

    working on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,100 @@
 var BinarySearchTree = function(value) {
+  var treeInstance = { value: value};
+  treeInstance.left = undefined;
+  treeInstance.right = undefined;
+
+  extend(treeInstance, BSTmethods);
+  return treeInstance;
 };
 
+
+// extend function
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+// methods declaration
+var BSTmethods = {};
+
+// insert: which accepts a value and places in the
+// tree in the correct position
+BSTmethods.insert = function (val) {
+
+
+  // inner recursive function (root)
+    // base case
+    // if val is less than current
+      // go to left side, if where we are is undefined
+        // val is inserted there
+      // else set new root
+    // else if val is greater than current
+      // go to right side, if where we are is undefined
+        // val is inserted there
+      // else set new root
+
+    // recursive case
+    // call function on new root value
+
+    if(!this.value) {
+      this.value = val;
+    } else {
+    var current = this;
+    while(current) {
+      if (val < current.value) {
+        if (current.left === undefined) {
+          current.left = { value: val};
+          break;
+        }
+        current = current.left;
+      } else if (val > current.value) {
+        if (current.right === undefined) {
+          current.right = { value: val};
+          break;
+        }
+        current = current.right;
+      } else {
+        break;
+      }
+    }
+  }
+};
+
+// contains: which accepts a value and returns a boolean
+// reflecting whether or not the value is contained in the tree
+BSTmethods.contains = function (val) {
+
+  var node = this;
+
+  var treeTraverse = function (node) {
+
+    if (this.value === val) {
+      return node;
+    } else {
+
+      if (val < this.value) {
+        node = this.left;
+        return treeTraverse(node);
+      } else {
+        node = this.right;
+        return treeTraverse(node);
+      }
+
+    }
+  };
+
+};
+
+// depthFirstLog: which accepts a callback and executes
+// it on every value contained in the tree
+BSTmethods.depthFirstLog = function (callback) {
+
+};
+
+// var myTree = BinarySearchTree(5);
+// myTree.insert(8);
+// console.log()
 
 /*
  * Complexity: What is the time complexity of the above functions?
